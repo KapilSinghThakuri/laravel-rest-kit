2b68426b4938ac6ab76c054cba7e9c5175a026f7
Kapil Thakuri <kapilsinghthakuri33@gmail.com>
2026-01-04 21:06:47 +0545

    feat(config): implement configurable variables for RestKit (, )

--- src/Middleware/ForceJsonResponse.php
+++ src/Middleware/ForceJsonResponse.php
@@ -4,12 +4,16 @@ namespace Kapilsinghthakuri\RestKit\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Kapilsinghthakuri\RestKit\RestKit;
 
 class ForceJsonResponse
 {
     public function handle(Request $request, Closure $next)
     {
-        $request->headers->set('Accept', 'application/json');
+        if (RestKit::config('force_json', true)) {
+            $request->headers->set('Accept', 'application/json');
+        }
+
         return $next($request);
     }
 }
--- src/Responses/ApiResponse.php
+++ src/Responses/ApiResponse.php
@@ -3,24 +3,43 @@
 namespace Kapilsinghthakuri\RestKit\Responses;
 
 use Illuminate\Http\JsonResponse;
+use Kapilsinghthakuri\RestKit\RestKit;
 
 class ApiResponse
 {
     public static function success($data = null, $message = 'Success!', int $status = 200): JsonResponse
     {
-        return new JsonResponse([
+        $response = [
             'status'  => 'success',
             'message' => $message,
             'data'    => $data,
-        ], $status);
+        ];
+
+        if (RestKit::config('debug', false)) {
+            $response['debug'] = [
+                'timestamp' => now(),
+                'memory' => memory_get_usage()
+            ];
+        }
+
+        return response()->json($response, $status);
     }
 
     public static function error($message = 'Error!', int $status = 500, $errors = null): JsonResponse
     {
-        return new JsonResponse([
+        $response = [
             'status'  => 'error',
             'message' => $message,
             'errors'  => $errors,
-        ], $status);
+        ];
+
+        if (RestKit::config('debug', false)) {
+            $response['debug'] = [
+                'timestamp' => now(),
+                'memory' => memory_get_usage()
+            ];
+        }
+
+        return response()->json($response, $status);
     }
 }
